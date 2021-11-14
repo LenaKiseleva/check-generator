4e800b3b6a74c790195289259490190789191eec
LenaKiseleva <ellen91981@mail.ru>
2021-11-14 22:55:29 +0300

    Disabled debug allowed all hosts

--- check_generator/settings.py
+++ check_generator/settings.py
@@ -10,9 +10,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
